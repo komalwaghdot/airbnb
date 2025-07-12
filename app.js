385c8df85e77aa420e04267fb9ad5a7a923275c3
komalwaghdot <komalwagh088@gmail.com>
2025-07-12 23:07:40 +0530

    Force HTTPS redirect on Render

--- app.js
+++ app.js
@@ -94,6 +94,14 @@ app.use((req,res,next)=>{
     next();
 });
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+});
+
+
 
 async function main() {
     await mongoose.connect(dbUrl);
